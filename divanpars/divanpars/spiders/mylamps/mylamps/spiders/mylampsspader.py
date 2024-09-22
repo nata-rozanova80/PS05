7f9c5a58640b274512ec8dd0f180ac4e20479169
NATALIA <nata.rozanova80@gmail.com>
2024-09-22 14:59:20 +0300

    mylamps commit

---
+++ divanpars/divanpars/spiders/mylamps/mylamps/spiders/mylampsspader.py
@@ -0,0 +1,49 @@
+import scrapy
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+class MySpider(scrapy.Spider):
+    name = 'myspider'
+    start_urls = ['https://www.divan.ru/product/svetilnik-potolochnyj-matthew']
+
+    def __init__(self, *args, **kwargs):
+        super(MySpider, self).__init__(*args, **kwargs)
+        # Инициализация драйвера
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    def parse(self, response):
+        # Открываем страницу с помощью Selenium
+        self.driver.get(response.url)
+        time.sleep(2)  # Ждем, чтобы страница загрузилась
+
+        # Извлечение данных
+        name = self.driver.find_element(By.CSS_SELECTOR, 'span[itemprop="name"]').text
+        price = self.driver.find_element(By.CSS_SELECTOR, 'meta[itemprop="price"]').get_attribute('content')
+        href = self.driver.find_element(By.CSS_SELECTOR, 'link[itemprop="url"]').get_attribute('href')
+
+        # Выводим результаты
+        self.log(f'Name: {name}')
+        self.log(f'Price: {price}')
+        self.log(f'Href: {href}')
+
+        # Закрываем драйвер после завершения
+        self.driver.quit()
+
+
+
+
+
+
+# import scrapy
+#
+#
+# class MylampsspaderSpider(scrapy.Spider):
+#     name = "mylampsspader"
+#     allowed_domains = ["divan.ru"]
+#     start_urls = ["https://divan.ru"]
+#
+#     def parse(self, response):
+#         pass
